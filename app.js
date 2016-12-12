85eccf57cf63cee4c320b7b80abca9f9fb660066
Tanya Reilly <tanya@noidea.dog>
2016-12-12 03:43:01 +0000

    Use redis to keep track of what's already been tweeted/retweeted. That lets us run more often without hammering twitter. Keeping the redis connection open meant an exciting adventure into the lands of async and promisification. In the end, the promising wasn't really necessary, but it's nicer looking code than the callbacks.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var MTA = require('mta-service-status');
 var TwitterPackage = require("twitter");
 var cheerio = require('cheerio');
+var RedisPackage = require('redis');
+var Promise = require('bluebird');
+
+var async = require("async");
+
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 var secret = require("./credentials_subwaystatus");
@@ -15,39 +20,38 @@ called credentials_subwaystatus.json containing:
 }
 */
 
+var redis = RedisPackage.createClient(); // default to localhos:6379
+
 var Twitter = new TwitterPackage(secret);
+
+Promise.promisifyAll(Twitter);
+
 var train = "F"  // The MTA feed doesn't include numbered trains.
 
-function tweet(text) {
-  Twitter.post('statuses/update',
-               {status: text},
-               function(error, tweet, response) {
-                 try {
-                   if (error) {
-                     console.warn("Error tweeting:", error);
-                     return
-                   }
-                   console.log("Tweeted:", tweet.text);
-                 } catch (e) {
-                   console.warn("Something went wrong", e);
-                 }
-               })
+function tweet(text, success_cb) {
+  console.log("tweeting", text);
+  var success = 0;
+  Twitter.postAsync('statuses/update', {status: this.text})
+    .then(function(error, tweet, response) {
+      console.log("Tweeted:", tweet);
+      success_cb(null);
+    }.bind({text: text}))
+    .catch(function (e) {
+      success_cb(e);
+    });
 }
 
-function retweet(tweetid) {
-  Twitter.post('statuses/retweet',
-               {id: tweetid},
-               function(error, tweet, response) {
-                 try {
-                   if (error) {
-                     console.warn("Error retweeting", tweetid, ":", error);
-                     return
-                   }
-                   console.log("Tweeted:", tweet.text);
-                 } catch (e) {
-                   console.warn("Something went wrong", e);
-                 }
-               })
+function retweet(tweetid, success_cb) {
+  console.log("retweeting", tweetid);
+  var success = 0;
+  Twitter.postAsync('statuses/retweet', {id: tweetid})
+    .then(function(error, tweet, response) {
+      console.log("Retweeted:", tweet);
+      success_cb(1);
+    }.bind({text: text}))
+    .catch(function (e) {
+      success_cb(e);
+    });
 }
 
 // Return an array of statuses to tweet. Since train status is batched by line,
@@ -85,65 +89,144 @@ function make_tweets(html) {
   return keep;
 }
 
+function set_redis (key) {
+  try {
+    redis.set(key, 1);
+    redis.expire(key, 43200);  //12h
+  } catch (e) {
+    console.log("Error writing to redis (key:", key, "):", e);
+  }
+}
+
+var get_statuses = function(callback) {
+  var statuses = []
+  MTA.getServiceStatus('subway', train).then(function(result) {
+    console.log("status of", train, "train:", result.status);
+
+    if (result.status === "GOOD SERVICE") {
+      console.log("good service");
+      callback(null, []);
+      return;
+    }
+    try {
+      statuses = make_tweets(result.html);
+    } catch (e) {
+      console.warn("Couldn't create tweets to post:", e);
+    }
+    callback(null, statuses);
+  });
+}
+
+var get_retweetable_ids = function(callback) {
+  var twittersearch = {
+    "NYCTSubway": " " + train + " ",
+    "amNewYork": train + " train",
+    "DNAinfoNY": train + " train",
+  }
+  var tried = 0;
+  for (user in twittersearch) {
+    var ids = []
+    var matchstring = twittersearch[user];
+
+    Twitter.get("statuses/user_timeline",
+      { screen_name: user, count: 50, exclude_replies: true, include_rts: false
+      }, function(error, data) {
+        var skipped = []
+        if (error) {
+          console.log("Error reading timeline for user", user, ":", error);
+        }
+        for (var i = 0; i < data.length ; i++) {
+          var text = data[i].text;
+          var id = data[i].id_str;
+          if (text.includes(matchstring)) {
+            ids.push(id);
+          }
+        }
+        if (++tried == Object.keys(twittersearch).length) {
+          callback(null, ids);
+        }
+      });
+  }
+}
+
+var tweet_mta_feed = function(statuses, callback) {
+  var tried = 0;
+  for (var i = 0; i < statuses.length; i++) {
+    text = statuses[i];
+    redis.exists(text, function(err, reply) {
+      if (reply == 1) {
+        console.log("Already tweeted [", this.text, "].Skipping.");
+      } else {
+        tweet(this.text, function(err) {
+          if (!err) {
+            set_redis(this.text);
+          } else {
+            console.log("got an error tweeting", this.text, ":", err);
+          }
+        }.bind({text: this.text}));
+      }
+      if (++tried == statuses.length) {
+        callback();
+      }
+    }.bind({text: text}));
+  }
+}
+
+var retweet_local_news = function(ids, callback) {
+  var tried = 0;
+  for (var i = 0; i < ids.length; i++) {
+    id = ids[i];
+
+    redis.exists(id, function(err, reply) {
+      if (reply == 1) {
+        console.log("Already retweeted [", this.id, "]. Skipping.");
+        if (++tried == ids.length) {
+          callback();
+        }
+      } else {
+        retweet(this.id, function(err) {
+          if (!err) {
+            set_redis(this.id);
+          } else {
+            console.log("Got an error retweeting:", err);
+          }
+          // TODO: Shouldn't need to check this twice in this function.
+          if (++tried == ids.length) {
+            callback();
+          }
+        }.bind( { id: this.id }));
+      }
+    }.bind( { id: id }));
+  }
+}
+
 /*** main ***/
 
-// First check whether the MTA service status has anything interesting.
-MTA.getServiceStatus('subway', train).then(function(result) {
-  console.log("status of", train, "train:", result.status);
-  if (result.status === "GOOD SERVICE") {
-    console.log("good service");
-    return;
-  }
-  try {
-    statuses = make_tweets(result.html);
-  } catch (e) {
-    console.warn("Couldn't create tweets to post:", e);
-    return
-  }
+redis.on('connect', function() {
+  console.log("Connected to redis.");
+});
+redis.on('error', function(err) {
+  console.log("Error connecting to redis:", err);
+});
 
-  for (var i = 0; i < statuses.length; i++) {
-    try {
-      summary = statuses[i]
-      tweet(summary);
-    } catch (e) {
-      console.warn("Couldn't tweet:", e);
-    }
-  }
-})
+redis.incr("subwaystatus:runs", function(err, reply) {
+  console.log("Beginning run", reply);
+});
 
-// users and what to search for in their statuses
-var twittersearch = {
-  "NYCTSubway": " " + train + " ",
-  "amNewYork": train + " train",
-  "DNAinfoNY": train + " train",
-}
-
-// Retweet anything a list of twitter accounts has to say about the train. We
-// could retweet arbitrarily, but people say some weird and unpleasant shit on
-// twitter.
-for (user in twittersearch) {
-  var matchstring = twittersearch[user];
-
-  (function(user, matchstring) {
-    Twitter.get("statuses/user_timeline",
-                {screen_name: user, count: 50, exclude_replies: true,
-                 include_rts: false}, function(error, data) {
-                  var skipped = []
-                  if (error) {
-                    console.log("Got an error:", error);
-                    return;
-                  }
-                  for (var i = 0; i < data.length ; i++) {
-                    var text = data[i].text;
-                    var id = data[i].id_str;
-                    if (text.includes(matchstring)) {
-                      retweet(id);
-                    } else {
-                      skipped.push(text);
-                    }
-                  }
-                  console.log("Skipped", skipped.length, "irrelevant tweets from",
-                              user + ".");
-               });
-  })(user, matchstring);
-}
+async.waterfall(
+[
+  function(callback) {
+    get_statuses(callback)
+  },
+  function(statuses, callback) {
+    tweet_mta_feed(statuses, callback)
+  },
+  function(callback) {
+    get_retweetable_ids(callback)
+  },
+  function(ids, callback) {
+    retweet_local_news(ids, callback)
+  },
+], function(err) { //Final callback
+    redis.quit();
+  });
